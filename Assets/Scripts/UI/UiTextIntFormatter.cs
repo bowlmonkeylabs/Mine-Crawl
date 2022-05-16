4f8bfcf945a4b3d3cb820218d4321343244f0803
Max Lepkowski <mlepkows@stevens.edu>
2022-05-16 01:04:13 -0400

    Add health and ore UI.

---
+++ Assets/Scripts/UI/UiTextIntFormatter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using BML.ScriptableObjectCore.Scripts.Variables;
+using TMPro;
+using UnityEngine;
+
+namespace BML.Scripts.UI
+{
+    public class UiTextIntFormatter : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _text;
+        [SerializeField] private string _formatString = "P0";
+        [SerializeField] private IntVariable _variable;
+
+        private void Awake()
+        {
+            UpdateText();
+            _variable.Subscribe(UpdateText);
+        }
+
+        private void OnDestroy()
+        {
+            _variable.Unsubscribe(UpdateText);
+        }
+
+        protected string GetFormattedValue()
+        {
+            return _variable.Value.ToString(_formatString);
+        }
+
+        protected void UpdateText()
+        {
+            _text.text = GetFormattedValue();
+        }
+    }
+}
