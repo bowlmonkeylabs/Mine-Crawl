b04a1ad1d6cb36349c21250c5a118ea793445c83
Dan C <djc5627@gmail.com>
2022-06-09 23:05:11 -0400

    Add and implement UpgradeManager. Connect store UI to UpgradeManager.

---
+++ Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BML.ScriptableObjectCore.Scripts.Events;
+using BML.ScriptableObjectCore.Scripts.Variables;
+using UnityEngine;
+
+namespace BML.Scripts
+{
+    public class UpgradeManager : MonoBehaviour
+    {
+        [SerializeField] private GameEvent _onBuyHealth;
+        [SerializeField] private IntVariable _playerHealth;
+        [SerializeField] private IntVariable _oreCount;
+        [SerializeField] private int healthIncrease = 1;
+        [SerializeField] private int oreCost = 10;
+
+        private void Awake()
+        {
+            _onBuyHealth.Subscribe(BuyHealth);
+        }
+
+        private void OnDestroy()
+        {
+            _onBuyHealth.Unsubscribe(BuyHealth);
+        }
+
+        private void BuyHealth()
+        {
+            if (_oreCount.Value < oreCost) return;
+            
+            _playerHealth.Value += healthIncrease;
+            _oreCount.Value -= oreCost;
+        }
+    }
+}
