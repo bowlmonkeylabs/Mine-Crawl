661b0051488907ff1d53f4e42b7ba944e4c285fd
merc37 <gabeajh@gmail.com>
2022-07-07 23:33:14 -0400

    Make enemies damage on trigger stay and faster

--- Assets/Scripts/DamageOnTrigger.cs
+++ Assets/Scripts/DamageOnTrigger.cs
@@ -8,14 +8,32 @@ namespace BML.Scripts
     {
         [SerializeField] private int _damage = 1;
         [SerializeField] private LayerMask _damageMask;
-        private void OnTriggerEnter(Collider other)
+        [SerializeField] private float _time = 2;
+
+        private float _lastTime = float.PositiveInfinity;
+        private bool _canDamage = false;
+
+        private void OnTriggerStay(Collider other)
         {
+            if(!_canDamage) {
+                return;
+            }
+
             GameObject otherObj = other.gameObject;
             if (!otherObj.IsInLayerMask(_damageMask)) return;
 
             Damageable damageable = otherObj.GetComponent<Damageable>();
-            if (damageable != null)
+            if (damageable != null) {
                 damageable.TakeDamage(_damage);
+                _canDamage = false;
+            }
+        }
+
+        private void Update() {
+            if(float.IsPositiveInfinity(_lastTime) || (Time.time - _lastTime) >= _time) {
+                _lastTime = Time.time;
+                _canDamage = true;
+            }
         }
     }
 }
