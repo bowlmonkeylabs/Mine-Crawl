7ed9aa243b69947f03c2df59ef960fbd0254ec8b
Dan C <djc5627@gmail.com>
2022-07-27 14:32:04 -0400

    MAKE SURE TO SYNC ANNEX. Add player and enemy footstep sounds.

---
+++ Assets/Scripts/FootstepController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BML.Scripts.Utils;
+using Pathfinding;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace BML.Scripts
+{
+    public class FootstepController : MonoBehaviour
+    {
+        [SerializeField] private float _distanceToStep = 1f;
+        [SerializeField] private UnityEvent _onFootstep;
+
+        private float distanceMoved;
+        private Vector3 lastPos;
+
+        private void OnEnable()
+        {
+            distanceMoved = 0;
+            lastPos = transform.position;
+        }
+
+        private void Update()
+        {
+            distanceMoved += Vector3.Magnitude((transform.position - lastPos));
+
+            if (distanceMoved > _distanceToStep)
+            {
+                distanceMoved -= _distanceToStep;
+                _onFootstep.Invoke();
+            }
+
+            lastPos = transform.position;
+        }
+    }
+}
