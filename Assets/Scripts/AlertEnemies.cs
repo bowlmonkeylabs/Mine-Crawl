f01c1e3b3ec8f6775cf6514489e0b8cdaf5bf634
Max Lepkowski <mlepkows@stevens.edu>
2022-08-14 12:32:47 -0400

    Hide enemy alert radius gizmo unless object directly selected in editor.

--- Assets/Scripts/AlertEnemies.cs
+++ Assets/Scripts/AlertEnemies.cs
@@ -18,7 +18,12 @@ namespace BML.Scripts
             }
         }
 
-        private void OnDrawGizmosSelected() {
+        private void OnDrawGizmosSelected()
+        {
+            #if UNITY_EDITOR
+            // Only draw if object is in the top-level of the selected (i.e. NOT a child of the selected object)
+            if (UnityEditor.Selection.activeGameObject != gameObject) return;
+            #endif
             Gizmos.color = Color.gray;
             Gizmos.DrawWireSphere(transform.position, _miningEnemyAlertRadius.Value);
         }
