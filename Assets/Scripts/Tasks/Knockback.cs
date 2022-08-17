f82f2f4ea23d6f0b65f17680ea2a58506478885f
Dan C <djc5627@gmail.com>
2022-08-17 14:31:24 -0400

    Add LoS check for enemies. Add transform scene reference for player camera root. Reduce enemy knockback.

--- Assets/Scripts/Tasks/Knockback.cs
+++ Assets/Scripts/Tasks/Knockback.cs
@@ -6,60 +6,65 @@ using BML.Scripts.Utils;
 using Pathfinding;
 using UnityEngine;
 
-[TaskCategory("AStarPro")]
-[TaskDescription("Knocksback a rich ai using rigidbody")]
-public class Knockback : Action
+namespace BML.Scripts.Tasks
 {
-    [SerializeField] private SharedVector3 KnockBackDirection;
-    [SerializeField] private SharedFloat KnockbackTime;
-    [SerializeField] private SharedFloat KnockbackMaxSpeed;
-    [SerializeField] private SharedFloat KnockbackMinSpeed;
-    [SerializeField] private SharedFloat KnockbackVerticalSpeed;
-	
-    [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private RichAI ai;
 
-    private float knockBackStartTime;
-    private CharacterController charController;
+    [TaskCategory("AStarPro")]
+    [TaskDescription("Knocksback a rich ai using rigidbody")]
+    public class Knockback : Action
+    {
+        [SerializeField] private SharedVector3 KnockBackDirection;
+        [SerializeField] private SharedFloat KnockbackTime;
+        [SerializeField] private SharedFloat KnockbackMaxSpeed;
+        [SerializeField] private SharedFloat KnockbackMinSpeed;
+        [SerializeField] private SharedFloat KnockbackVerticalSpeed;
 
-    public override void OnStart()
-    {
-        charController = transform.GetComponentInChildren<CharacterController>();
-        knockBackStartTime = Time.time;
-        ai.enabled = false;
-        charController.enabled = true;
-    }
-	
-    public override void OnEnd()
-    {
-        ai.enabled = true;
-        //_rigidbody.isKinematic = true;
-        charController.enabled = false;
-    }
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private RichAI ai;
 
-    public override TaskStatus OnUpdate()
-    {
-        if (knockBackStartTime + KnockbackTime.Value > Time.time)
+        private float knockBackStartTime;
+        private CharacterController charController;
+
+        public override void OnStart()
         {
-            HandleKnockBack();
-            return TaskStatus.Running;
+            charController = transform.GetComponentInChildren<CharacterController>();
+            knockBackStartTime = Time.time;
+            ai.enabled = false;
+            charController.enabled = true;
         }
 
-        return TaskStatus.Success;
+        public override void OnEnd()
+        {
+            ai.enabled = true;
+            //_rigidbody.isKinematic = true;
+            charController.enabled = false;
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            if (knockBackStartTime + KnockbackTime.Value > Time.time)
+            {
+                HandleKnockBack();
+                return TaskStatus.Running;
+            }
+
+            return TaskStatus.Success;
+        }
+
+        private void HandleKnockBack()
+        {
+            var percentComplete = (Time.time - knockBackStartTime) / KnockbackTime.Value;
+            var horizontalSpeed = Mathf.SmoothStep(KnockbackMaxSpeed.Value, KnockbackMinSpeed.Value, percentComplete);
+            var horizontalVelocity = horizontalSpeed * KnockBackDirection.Value.xoz();
+
+            var verticalSpeed = Mathf.SmoothStep(KnockbackVerticalSpeed.Value, -KnockbackVerticalSpeed.Value,
+                percentComplete);
+            var verticalVelocity = verticalSpeed * Vector3.up;
+
+            horizontalVelocity += verticalVelocity;
+
+            charController.Move(horizontalVelocity * Time.deltaTime);
+        }
+
     }
-    
-    private void HandleKnockBack()
-    {
-        var percentComplete = (Time.time - knockBackStartTime) / KnockbackTime.Value;
-        var horizontalSpeed = Mathf.SmoothStep(KnockbackMaxSpeed.Value, KnockbackMinSpeed.Value,  percentComplete);
-        var horizontalVelocity = horizontalSpeed * KnockBackDirection.Value.xoz();
-        
-        var verticalSpeed = Mathf.SmoothStep(KnockbackVerticalSpeed.Value, -KnockbackVerticalSpeed.Value,  percentComplete);
-        var verticalVelocity = verticalSpeed * Vector3.up;
-
-        horizontalVelocity += verticalVelocity;
-
-        charController.Move(horizontalVelocity * Time.deltaTime);
-    }
-
 }
---
+++ Assets/Scripts/Tasks/LineOfSight.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using BML.ScriptableObjectCore.Scripts.SceneReferences;
+using BML.Scripts.Utils;
+using UnityEngine;
+
+namespace BML.Scripts.Tasks
+{
+    [TaskCategory("Physics")]
+    [TaskDescription("Check if object is within line of sight")]
+    public class LineOfSight : Conditional
+    {
+        [SerializeField] private float _checkDelay = .05f;
+        
+        [SerializeField] [BehaviorDesigner.Runtime.Tasks.Tooltip("Mask of everything raycast can interact with (including obstacles and target)")] 
+        private LayerMask _hitLayerMask;
+        
+        [SerializeField] [BehaviorDesigner.Runtime.Tasks.Tooltip("Mask of layers that are considered obstacles")] 
+        private LayerMask _obstacleLayerMask;
+        
+        [SerializeField] private Transform _origin;
+        [SerializeField] private TransformSceneReference _target;
+
+        private float lastCheckTime = Mathf.NegativeInfinity;
+        
+        public override TaskStatus OnUpdate()
+        {
+            if (lastCheckTime + _checkDelay > Time.time)
+                return TaskStatus.Failure;
+
+            RaycastHit hit;
+            Vector3 dir = (_target.Value.position - _origin.position).normalized;
+            if (Physics.Raycast(_origin.position, dir, out hit, Mathf.Infinity, _hitLayerMask, QueryTriggerInteraction.Ignore))
+            {
+                if (hit.collider.gameObject.IsInLayerMask(_obstacleLayerMask))
+                    return TaskStatus.Failure;
+
+                return TaskStatus.Success;
+            }
+
+            return TaskStatus.Failure;
+        }
+    }
+}
