383e7215c1b96a63b149b470834f3b07cf6b349e
Max Lepkowski <mlepkows@stevens.edu>
2022-08-18 14:47:30 -0400

    dev/nav (#11)
    * Add compass.
    
    * Add FOV setting.
    
    * Add compass target to main scene.

---
+++ Assets/Scripts/CinemachineFovSetter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BML.ScriptableObjectCore.Scripts.Variables;
+using Cinemachine;
+using UnityEngine;
+
+namespace BML.Scripts
+{
+    public class CinemachineFovSetter : MonoBehaviour
+    {
+        #region Inspector
+
+        [SerializeField] private CinemachineVirtualCamera _camera;
+        [SerializeField] private FloatReference _fovValue;
+
+        #endregion
+        
+        #region Unity lifecycle
+
+        private void OnEnable()
+        {
+            SetCinemachineCameraFov();
+            _fovValue.Subscribe(SetCinemachineCameraFov);
+        }
+        
+        private void OnDisable()
+        {
+            _fovValue.Unsubscribe(SetCinemachineCameraFov);
+        }
+
+        #endregion
+
+        private void SetCinemachineCameraFov()
+        {
+            _camera.m_Lens.FieldOfView = _fovValue.Value;
+        }
+    }
+}
