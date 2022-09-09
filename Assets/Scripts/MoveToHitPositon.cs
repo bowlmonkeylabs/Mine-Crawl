3005acba439020910ded36230179870e46f156f6
Dan C <djc5627@gmail.com>
2022-09-08 20:56:09 -0400

    SYNC ANNEX NEEDED. Add sounds and particles for hitting exit ore shield.

---
+++ Assets/Scripts/MoveToHitPositon.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace BML.Scripts
+{
+    public class MoveToHitPositon : MonoBehaviour
+    {
+        public void Move(HitInfo hitInfo)
+        {
+            transform.position = hitInfo.HitPositon;
+        }
+    }
+}
