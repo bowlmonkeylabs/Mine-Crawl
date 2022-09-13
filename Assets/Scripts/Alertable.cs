9ee95c8d51f348a4c1e51507b0e9e2c587e3e997
Dan C <djc5627@gmail.com>
2022-09-13 16:12:33 -0400

    Enhanced sentinel enemy.

--- Assets/Scripts/Alertable.cs
+++ Assets/Scripts/Alertable.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 namespace BML.Scripts
 {
     [RequireComponent(typeof(BehaviorDesigner.Runtime.BehaviorTree))]
     public class Alertable : MonoBehaviour
     {
-        [SerializeField] private BehaviorDesigner.Runtime.BehaviorTree behaviorTree;
+        [FormerlySerializedAs("behaviorTree")] [SerializeField] private BehaviorDesigner.Runtime.BehaviorTree _behaviorTree;
+        [SerializeField] private UnityEvent _onAlerted;
 
         public void SetAlerted() {
-            behaviorTree.SendEvent("SetAlerted");
+            _behaviorTree.SendEvent("SetAlerted");
+            _onAlerted.Invoke();
         }
 
     }
