724bb33b824acd53ce5879ff9daa11f2623567a3
Dan C <djc5627@gmail.com>
2022-10-05 14:32:56 -0400

    Add sliders for Music and Sfx volume. Fix slime footsteps when not grounded. Add music to level which shuffles based on level seed. Tweaks to audio (sync annex too)

---
+++ Assets/Scripts/PlayRandomClip.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using BML.Scripts.CaveV2;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace BML.Scripts
+{
+    public class PlayRandomClip : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private CaveGenComponentV2 _caveGenComponent;
+        [SerializeField] private List<AudioClip> _clips = new List<AudioClip>();
+
+        private void Start()
+        {
+            Random.InitState(_caveGenComponent.CaveGenParams.Seed);
+            int randomIndex = Random.Range(0, _clips.Count);
+            _audioSource.clip = _clips[randomIndex];
+            _audioSource.Play();
+        }
+    }
+}
