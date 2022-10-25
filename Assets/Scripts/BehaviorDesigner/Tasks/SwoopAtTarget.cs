9f4de3fca0c77e33df6d3ed853f18821c3672f64
Dan C <djc5627@gmail.com>
2022-10-25 12:58:53 -0400

    Implement bat swoop.

--- Assets/Scripts/BehaviorDesigner/Tasks/SwoopAtTarget.cs
+++ Assets/Scripts/BehaviorDesigner/Tasks/SwoopAtTarget.cs
@@ -16,11 +16,16 @@ namespace BML.Scripts.Tasks
         [SerializeField] private SharedTransform _swoopTransform;
         [SerializeField] private TransformSceneReference _targetRef;
         [SerializeField] private SharedFloat _swoopTime;
+        [SerializeField] private SharedFloat _swoopYOffset;
+        [SerializeField] private AnimationCurve _horizontalMoveCurve;
+        [SerializeField] private AnimationCurve _verticalMoveCurve;
         [SerializeField] private AIBase ai;
 
         private float swoopStartTime;
         private Vector3 startPos;
         private Vector3 destination;
+        private Vector3 horizontalDir, verticalDir;
+        private float horizontalDelta, verticalDelta;
         private CharacterController charController;
 
         public override void OnStart()
@@ -30,7 +35,11 @@ namespace BML.Scripts.Tasks
             ai.enabled = false;
             charController.enabled = true;
             startPos = _swoopTransform.Value.position;
-            destination = _targetRef.Value.position; //Store player pos at target
+            destination = _targetRef.Value.position + _swoopYOffset.Value * Vector3.up; //Store player pos at target
+            horizontalDir = (destination.xoz() - startPos.xoz()).normalized;
+            horizontalDelta = (destination.xoz() - startPos.xoz()).magnitude;
+            verticalDir = Vector3.down;
+            verticalDelta = Mathf.Abs(destination.y - startPos.y);
         }
         
         public override void OnEnd()
@@ -53,17 +62,12 @@ namespace BML.Scripts.Tasks
         public void HandleSwoop()
         {
             var percentComplete = (Time.time - swoopStartTime) / _swoopTime.Value;
-            
-            //TODO: only calc next 2 once
-            var horizontalDir = (destination.xoz() - startPos.xoz()).normalized;
-            var horizontalDelta = (destination.xoz() - startPos.xoz()).magnitude;
-            var horizontalTarget = startPos.xoz() + horizontalDir * horizontalDelta * percentComplete;
-            var horizontalMove = (horizontalTarget - _swoopTransform.Value.position.xoz()).magnitude * horizontalDir;
 
-            var veritcalDir = Vector3.down;
-            var verticalDelta = Mathf.Abs(destination.y - startPos.y);
-            var verticalTarget = startPos.oyo() + veritcalDir * verticalDelta * percentComplete;
-            var verticalMove = (verticalTarget - _swoopTransform.Value.position.oyo()).magnitude * veritcalDir;
+            var horizontalTarget = startPos.xoz() + horizontalDir * horizontalDelta * _horizontalMoveCurve.Evaluate(percentComplete);
+            var horizontalMove = (horizontalTarget - _swoopTransform.Value.position.xoz()).magnitude * horizontalDir;
+            
+            var verticalTarget = startPos.oyo() + verticalDir * verticalDelta * _verticalMoveCurve.Evaluate(percentComplete);
+            var verticalMove = (verticalTarget - _swoopTransform.Value.position.oyo()).magnitude * verticalDir;
 
             charController.Move(horizontalMove + verticalMove);
         }
