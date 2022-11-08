465abd3625f81628e2eb8d1b2ff601dc0fda7605
merc37 <gabeajh@gmail.com>
2022-11-07 22:13:11 -0500

    Build fixes: shape renderer, quit button, bomb button

---
+++ Assets/Scripts/ApplicationActions.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BML.Scripts.Utils
+{
+    public class ApplicationActions : MonoBehaviour
+    {
+        public void DoQuit() {
+            Application.Quit();
+        }
+    }
+}
