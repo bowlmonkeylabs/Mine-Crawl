4129a10da94e96e2fb8ce34b810d5df637274440
Max Lepkowski <mlepkows@stevens.edu>
2022-11-09 23:50:07 -0500

    Implement pickup drops for common rock.

--- Assets/Scripts/Ore/LootRandomizer.cs
+++ Assets/Scripts/Ore/LootRandomizer.cs
@@ -1,11 +1,14 @@
 ﻿using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace BML.Scripts
 {
     public class LootRandomizer : MonoBehaviour
     {
         [SerializeField] private LootTable _lootTable;
+
+        [SerializeField] private UnityEvent<GameObject> _onDrop;
 
         [ShowInInspector] private float _randomRoll;
 
@@ -16,7 +19,8 @@ namespace BML.Scripts
         
         public void Drop()
         {
-            _lootTable.Evaluate(_randomRoll);
+            var dropPrefabs = _lootTable.Evaluate(_randomRoll);
+            dropPrefabs.ForEach(prefab => _onDrop?.Invoke(prefab));
         }
     }
 }
