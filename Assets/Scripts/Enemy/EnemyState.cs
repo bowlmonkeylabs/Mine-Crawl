e550cd19683c7d62ee5df33541e447c9bfb63a71
Dan C <djc5627@gmail.com>
2022-12-01 13:39:39 -0500

    LoS external behavior working with every enemy.

--- Assets/Scripts/Enemy/EnemyState.cs
+++ Assets/Scripts/Enemy/EnemyState.cs
@@ -7,10 +7,14 @@ namespace BML.Scripts.Enemy
     public class EnemyState : MonoBehaviour
     {
         // [SerializeField] private Enemy Type
+        [SerializeField] private BehaviorDesigner.Runtime.BehaviorTree _behaviorTree;
         [SerializeField] private Difficulty _difficulty;
         [SerializeField] private CaveNodeDataDebugComponent _currentNode;
         [SerializeField] private AggroState _aggro;
         [SerializeField] private int _nodeDistanceFromPlayer;
+        [SerializeField] private float _updateFrequency = .25f;
+
+        private float lastUpdateTime = Mathf.NegativeInfinity;
 
         [Serializable]
         enum Difficulty
