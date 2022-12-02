672b149916f30a37d381419ad9dd5715bbc9f3e7
Dan C <djc5627@gmail.com>
2022-12-02 00:17:39 -0500

    EnemyState script tracks which rooms and tunnels enemies are in.

--- Assets/Scripts/Enemy/EnemyState.cs
+++ Assets/Scripts/Enemy/EnemyState.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using BML.Scripts.CaveV2.CaveGraph;
+using BML.Scripts.CaveV2.CaveGraph.NodeData;
+using BML.Scripts.Utils;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -9,6 +13,7 @@ namespace BML.Scripts.Enemy
     {
         // [SerializeField] private Enemy Type
         [SerializeField] private BehaviorDesigner.Runtime.BehaviorTree _behaviorTree;
+        [SerializeField] private LayerMask _nodeBoundsLayerMask;
         [SerializeField] private Difficulty _difficulty;
         [SerializeField] private CaveNodeDataDebugComponent _currentNode;
         [SerializeField] private AggroState _aggro;
@@ -22,6 +27,9 @@ namespace BML.Scripts.Enemy
         public bool IsPlayerInLoS { get => isPlayerInLoS; set => isPlayerInLoS = value; }
         
         private float lastUpdateTime = Mathf.NegativeInfinity;
+        
+        private Dictionary<Collider, CaveNodeData> _currentNodes;
+        private Dictionary<Collider, CaveNodeConnectionData> _currentNodeConnections;
 
         #region Enums
 
@@ -45,6 +53,77 @@ namespace BML.Scripts.Enemy
 
         #region UnityLifecyle
 
+        private void Awake()
+        {
+            _currentNodes = new Dictionary<Collider, CaveNodeData>();
+            _currentNodeConnections = new Dictionary<Collider, CaveNodeConnectionData>();
+        }
+
+        private void Update()
+        {
+            UpdateAggroState();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            bool isNodeBoundsLayer = other.gameObject.IsInLayerMask(_nodeBoundsLayerMask);
+            if (isNodeBoundsLayer)
+            {
+                bool isAlreadyEntered =
+                    (_currentNodes.ContainsKey(other) || _currentNodeConnections.ContainsKey(other));
+                
+                if (isAlreadyEntered)
+                {
+                    return;
+                }
+                
+                var caveNodeDataComponent = other.GetComponentInParent<CaveNodeDataDebugComponent>();
+                if (caveNodeDataComponent != null)
+                {
+                    var caveNodeData = caveNodeDataComponent.CaveNodeData;
+                    if (caveNodeData == null)
+                    {
+                        return;
+                    }
+                        
+                    _currentNodes.Add(other, caveNodeData);
+                }
+                else
+                {
+                    var caveNodeConnectionDataComponent =
+                        other.GetComponentInParent<CaveNodeConnectionDataDebugComponent>();
+                    if (caveNodeConnectionDataComponent == null)
+                    {
+                        return;
+                    }
+                        
+                    var caveNodeConnectionData = caveNodeConnectionDataComponent.CaveNodeConnectionData;
+                    if (caveNodeConnectionData == null)
+                    {
+                        return;
+                    }
+
+                    _currentNodeConnections.Add(other, caveNodeConnectionData);
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            bool isNodeBoundsLayer = other.gameObject.IsInLayerMask(_nodeBoundsLayerMask);
+            if (isNodeBoundsLayer)
+            {
+                if (_currentNodes.ContainsKey(other))
+                {
+                    _currentNodes.Remove(other);
+                }
+                else if (_currentNodeConnections.ContainsKey(other))
+                {
+                    _currentNodeConnections.Remove(other);
+                }
+            }
+        }
+
         private void OnDrawGizmosSelected()
         {
             switch (_aggro)
@@ -63,12 +142,21 @@ namespace BML.Scripts.Enemy
                     break;
             }
 
-            Gizmos.DrawSphere(transform.position + Vector3.up * 1.5f, .2f);
-        }
+            Vector3 gizmoPivot = transform.position + Vector3.up * 1.5f;
+            Gizmos.DrawSphere(gizmoPivot, .2f);
+            
+            // Draw line from sphere to room/tunnel centers it belongs to
+            Gizmos.color = Color.white;
 
-        private void Update()
-        {
-            UpdateAggroState();
+            foreach (var node in _currentNodes)
+            {
+                Gizmos.DrawLine(gizmoPivot, node.Value.GameObject.transform.position);
+            }
+            
+            foreach (var edge in _currentNodeConnections)
+            {
+                Gizmos.DrawLine(gizmoPivot, edge.Value.GameObject.transform.position);
+            }
         }
 
         #endregion
