743e0c3ef0fec7c98540c8600fe5bbf6d12c838b
Dan C <djc5627@gmail.com>
2022-12-05 20:10:18 -0500

    Add and implement ambient brightness slider in options.

---
+++ Assets/Scripts/Lighting/LightManager.cs
@@ -0,0 +1,32 @@
+﻿using BML.ScriptableObjectCore.Scripts.Variables;
+using BML.ScriptableObjectCore.Scripts.Variables.SafeValueReferences;
+using UnityEngine;
+
+namespace Lighting
+{
+    public class LightManager : MonoBehaviour
+    {
+        [SerializeField] private FloatVariable _brightnessSetting;
+        
+        #region Unity lifecycle
+
+        private void OnEnable()
+        {
+            SetAmbientLight();
+            _brightnessSetting.Subscribe(SetAmbientLight);
+        }
+        
+        private void OnDisable()
+        {
+            _brightnessSetting.Unsubscribe(SetAmbientLight);
+        }
+
+        #endregion
+        
+        public void SetAmbientLight()
+        {
+            float c = _brightnessSetting.Value / 255f;
+            RenderSettings.ambientLight = new Color(c, c, c, 1f);
+        }
+    }
+}
