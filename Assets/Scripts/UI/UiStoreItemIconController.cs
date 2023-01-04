89f467afdba7e45a05720b5ba210f0079c4189b1
Max Lepkowski <mlepkows@stevens.edu>
2023-01-04 13:47:17 -0500

    Implement inventory icon counter. Split Aero Wedge upgrade into three levels.

--- Assets/Scripts/UI/UiStoreItemIconController.cs
+++ Assets/Scripts/UI/UiStoreItemIconController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using Sirenix.Utilities;
 using BML.Scripts.Store;
 using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,12 +12,43 @@ namespace BML.Scripts.UI
     {
         [SerializeField] private StoreItem _storeItem;
         [SerializeField] private Image _iconImage;
+        [SerializeField] private CountMode _countMode = CountMode.None;
+        [SerializeField] private GameObject _countGameObject;
+        [SerializeField] private TMP_Text _countText;
         [SerializeField] private UiStoreItemDetailController _uiStoreItemDetailController;
 
+        public enum CountMode
+        {
+            None,
+            Inventory,
+            BuyAmount,
+        }
+        
         public void Init(StoreItem storeItem) 
         {
             _storeItem = storeItem;
             _iconImage.sprite = _storeItem._itemIcon;
+            
+            if (_countMode == CountMode.None)
+            {
+                _countGameObject.SetActive(false);
+            }
+            else
+            {
+                int countValue = 0;
+                if (_countMode == CountMode.Inventory)
+                {
+                    countValue = _storeItem._playerInventoryAmount.Value;
+                }
+                else if (_countMode == CountMode.BuyAmount)
+                {
+                    countValue = _storeItem._buyAmount;
+                }
+                
+                bool doShowCount = (countValue > 1);
+                _countGameObject.SetActive(doShowCount);
+                _countText.text = countValue.ToString();
+            }
         }
 
         public void SetStoreItemToSelected() {
