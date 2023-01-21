df52669767f6a83ca66c966c0f2db7fe7ec7f297
Dan C <djc5627@gmail.com>
2023-01-21 00:13:02 -0500

    Switch to mecanim for pickaxe animations. New swing animation that scales with swing speed level.

---
+++ Assets/Scripts/Player/SetAnimationSpeed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BML.ScriptableObjectCore.Scripts.Variables;
+using UnityEngine;
+
+namespace BML.Scripts.Player
+{
+    public class SetAnimationSpeed : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private string _parameterName;
+        [SerializeField] private EvaluateCurveVariable _scale;
+
+        private void LateUpdate()
+        {
+            _animator.SetFloat(_parameterName, _scale.Value);
+        }
+    }
+}
