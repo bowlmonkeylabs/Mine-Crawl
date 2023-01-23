6fc8d5bf683e825d13ba7db026301b974327632f
djc5627 <djc5627@gmail.com>
2023-01-23 13:37:48 -0500

    Pickaxe swing effect waits for animation to hit. (#54)

---
+++ Assets/Scripts/Animation/AnimEventReceiver.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sirenix.Utilities;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace BML.Scripts.Animation
+{
+    public class AnimEventReceiver : MonoBehaviour
+    {
+        [SerializeField] private List<AnimEvent> _animEvents = new List<AnimEvent>();
+
+        [System.Serializable]
+        public class AnimEvent
+        {
+            public string EventName;
+            public UnityEvent Response;
+        }
+
+        public void ReceiveAnimEvent(string name)
+        {
+            if (_animEvents.IsNullOrEmpty())
+            {
+                Debug.LogWarning("Tried to receive AnimEvent but Anim Event List is empty or null!");
+                return;
+            }
+
+            AnimEvent animEvent = _animEvents.First(a => a.EventName.Equals(name));
+
+            if (animEvent == null)
+            {
+                Debug.LogWarning("No Anim Event exits with the received name!");
+                return;
+            }
+
+            animEvent.Response.Invoke();
+        }
+    }
+}
