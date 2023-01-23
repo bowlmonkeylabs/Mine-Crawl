9b6432d18f005edfef8dac1c5e65e18cdf16edf0
Dan C <djc5627@gmail.com>
2023-01-23 11:43:54 -0500

    Add fall sway and rotation.

--- Assets/Scripts/Player/SwayAndBob.cs
+++ Assets/Scripts/Player/SwayAndBob.cs
@@ -1,4 +1,5 @@
-﻿using BML.ScriptableObjectCore.Scripts.Variables;
+﻿using System;
+using BML.ScriptableObjectCore.Scripts.Variables;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -17,6 +18,8 @@ namespace BML.Scripts.Player
         [SerializeField] private bool _swayRotation = true;
         [SerializeField] private bool _bobOffset = true;
         [SerializeField] private bool _bobSway = true;
+        [SerializeField] private bool _fallOffset = true;
+        [SerializeField] private bool _fallRotation = true;
         
         [Header("Sway")]
         [SerializeField] private float _step = 0.01f;
@@ -30,9 +33,10 @@ namespace BML.Scripts.Player
 
         [Header("Bobbing")] 
         [SerializeField] private float _speedCurve;
+        [SerializeField] private float _speedMultiplier = 1f;
         [SerializeField] private float _idleMultiplier;
         [Tooltip("Want this set to 180 for left-hand items")]
-        [SerializeField] [Range(0f, 360f)] private float _bobPhaseOffset;
+        [SerializeField] [Range(0f, 360f)] private float _bobPhaseOffsetX;
         
         [Tooltip("Maximum limits of travel from move input")]
         [SerializeField] private Vector3 _travelLimit = Vector3.one * 0.025f;
@@ -41,33 +45,43 @@ namespace BML.Scripts.Player
         [SerializeField] private Vector3 _bobLimit = Vector3.one * 0.01f;
         
         private Vector3 bobPosition;
-        private float curveSin { get => Mathf.Sin((_speedCurve + _bobPhaseOffset)); }
-        private float curveCos { get => Mathf.Cos((_speedCurve + _bobPhaseOffset)); }
+        private float curveSin { get => Mathf.Sin((_speedCurve)); }
+        private float curveCos { get => Mathf.Cos((_speedCurve)); }
 
+        private Vector3 originalPosition;
 
         [Header("BobRotation")]
-        [SerializeField] [FormerlySerializedAs("multiplier")] private Vector3 _multiplier;
+        [SerializeField] private Vector3 _bobMultiplier;
 
         private Vector3 bobEulerRot;
+        
+        [Header("FallOffset")]
+        [SerializeField] private Vector3 _travelLimitFall = Vector3.one * 0.1f;
+        private Vector3 fallPosition;
+
+        
+        [Header("FallRotation")]
+        [SerializeField] private float _fallMultiplier;
+        private Vector3 fallEulerRot;
+
+        private void OnEnable()
+        {
+            originalPosition = transform.localPosition;
+        }
 
         private void Update()
         {
-            GetInput();
-
             Sway();
             SwayRotation();
 
-            _speedCurve += Time.deltaTime * (_isGrounded.Value ? _currentVelocity.Value.magnitude : 1f) + 0.01f;
+            _speedCurve += Time.deltaTime * _speedMultiplier * (_isGrounded.Value ? _currentVelocity.Value.magnitude : 1f);
             
             BobOffset();
             BobRotation();
+            FallOffset();
+            FallRotation();
 
             CompositePositionRotation();
-        }
-
-        private void GetInput()
-        {
-            
         }
 
         private void Sway()
@@ -87,14 +101,14 @@ namespace BML.Scripts.Player
             if (!_swayRotation) { swayEulerRot = Vector3.zero; return; }
 
             Vector3 invertLook = _mouseInput.Value * -_rotationStep;
-            invertLook.x = Mathf.Clamp(invertLook.x, -_maxStepDistance, _maxStepDistance);
-            invertLook.y = Mathf.Clamp(invertLook.y, -_maxStepDistance, _maxStepDistance);
+            invertLook.x = Mathf.Clamp(invertLook.x, -_maxRotationStep, _maxRotationStep);
+            invertLook.y = Mathf.Clamp(invertLook.y, -_maxRotationStep, _maxRotationStep);
             invertLook.y = -invertLook.y;
             
             //TODO: Try making the Z (roll axis) negative so weapon top lags behind
             swayEulerRot = new Vector3(invertLook.y, invertLook.x, invertLook.x);
         }
-        
+
         private void BobOffset()
         {
             if (!_bobOffset) { bobPosition = Vector3.zero; return; }
@@ -104,13 +118,14 @@ namespace BML.Scripts.Player
                 - (_moveInput.Value.x * _travelLimit.x);
             
             bobPosition.y = 
-                (curveCos * _bobLimit.y * (_isGrounded.Value ? 1 : 0)) * _idleMultiplier
+                (curveSin * _bobLimit.y * (_isGrounded.Value ? 1 : 0)) * _idleMultiplier
                 - (_moveInput.Value.y * _travelLimit.y);
 
             bobPosition.z = 
                 -(_moveInput.Value.y * _travelLimit.z * _idleMultiplier);
         }
 
+        [Header("Debug")]
         public bool rotX;
         public bool rotY;
         public bool rotZ;
@@ -119,15 +134,28 @@ namespace BML.Scripts.Player
         {
             if (!_bobSway) { bobEulerRot = Vector3.zero; return; }
             
-            bobEulerRot.x = (_moveInput.Value != Vector2.zero ? _multiplier.x * (Mathf.Sin(2 * _speedCurve + _bobPhaseOffset)) :
-                                                            _idleMultiplier * (Mathf.Sin(2 * _speedCurve + _bobPhaseOffset) / 2f));
-            bobEulerRot.y = (_moveInput.Value != Vector2.zero ? _multiplier.y * curveCos : 0);
-            bobEulerRot.z = (_moveInput.Value != Vector2.zero ? _multiplier.z * curveCos * _moveInput.Value.x : 0);
+            bobEulerRot.x = (_moveInput.Value != Vector2.zero ? _bobMultiplier.x * (Mathf.Sin(2 * _speedCurve + _bobPhaseOffsetX)) :
+                                                            _idleMultiplier * (Mathf.Sin(2 * _speedCurve + _bobPhaseOffsetX) / 2f));
+            bobEulerRot.y = (_moveInput.Value != Vector2.zero ? _bobMultiplier.y * curveCos : 0);
+            bobEulerRot.z = (_moveInput.Value != Vector2.zero ? _bobMultiplier.z * curveCos * _moveInput.Value.x : 0);
 
-            if (rotX) bobEulerRot.x = 0f;
-            if (rotY) bobEulerRot.y = 0f;
-            if (rotZ) bobEulerRot.z = 0f;
+            if (!rotX) bobEulerRot.x = 0f;
+            if (!rotY) bobEulerRot.y = 0f;
+            if (!rotZ) bobEulerRot.z = 0f;
+        }
+        
+        private void FallOffset()
+        {
+            if (!_fallOffset) { fallPosition = Vector3.zero; return; }
+            
+            fallPosition.y = - (_currentVelocity.Value.y * _travelLimitFall.y);
+        }
 
+        private void FallRotation()
+        {
+            if (!_fallRotation) { fallEulerRot = Vector3.zero; return; }
+
+            fallEulerRot.x = (_currentVelocity.Value.y * _fallMultiplier);
         }
 
         private float smooth = 10f;
@@ -137,13 +165,13 @@ namespace BML.Scripts.Player
             // Position
             transform.localPosition =
                 Vector3.Lerp(transform.localPosition,
-                    swayPos + bobPosition,
+                    originalPosition + swayPos + bobPosition + fallPosition,
                     Time.deltaTime * smooth);
             
             // Rotation
             transform.localRotation =
                 Quaternion.Slerp(transform.localRotation,
-                    Quaternion.Euler(swayEulerRot) * Quaternion.Euler(bobEulerRot),
+                    Quaternion.Euler(swayEulerRot) * Quaternion.Euler(bobEulerRot) * Quaternion.Euler(fallEulerRot),
                     Time.deltaTime * smoothRot);
         }
     }
