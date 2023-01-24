25d7ba234784fe150cd3f635daae7734cd39e4cb
Max Lepkowski <mlepkows@stevens.edu>
2023-01-24 16:32:38 -0500

    Add loading screen for main game.

---
+++ Assets/Scripts/UI/UiCustomLoadingScreen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using AdvancedSceneManager.Callbacks;
+using AdvancedSceneManager.Core;
+using AdvancedSceneManager.Utility;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BML.Scripts.UI
+{
+    public class UiCustomLoadingScreen : LoadingScreen
+    {
+        #region Inspector
+
+        [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField, Min(0f)] private float _fadeOpen = 0f;
+        [SerializeField, Min(0f)] private float _fadeClose = 0f;
+        [SerializeField] private Slider _sliderProgress;
+
+        #endregion
+
+        #region Unity lifecycle
+
+        private void Update()
+        {
+            if (operation != null)
+            {
+                _sliderProgress.value = operation.totalProgress;
+            }
+        }
+
+        #endregion
+
+        public override IEnumerator OnOpen(SceneOperation operation)
+        {
+            yield return _canvasGroup.Fade(1, _fadeOpen);
+        }
+
+        public override IEnumerator OnClose(SceneOperation operation)
+        {
+            yield return _canvasGroup.Fade(0, _fadeClose);
+        }
+        
+    }
+}
