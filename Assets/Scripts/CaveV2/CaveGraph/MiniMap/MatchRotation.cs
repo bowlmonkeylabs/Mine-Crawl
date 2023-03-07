5c786d250da85dc0da159c0c6f222bd941aabdc3
Max Lepkowski <mlepkows@stevens.edu>
2023-03-07 15:02:57 -0500

    Add player location indicator.

---
+++ Assets/Scripts/CaveV2/CaveGraph/MiniMap/MatchRotation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BML.ScriptableObjectCore.Scripts.Variables.SafeValueReferences;
+using UnityEngine;
+
+namespace BML.Scripts.CaveV2.CaveGraph.Minimap
+{
+    public class MatchRotation : MonoBehaviour
+    {
+        #region Inspector
+
+        [SerializeField] private SafeTransformValueReference _target;
+        [SerializeField] private SafeTransformValueReference _transform;
+        [SerializeField] private bool _readLocalRotation = false;
+        [SerializeField] private bool _setLocalRotation = false;
+
+        #endregion
+
+        #region Unity lifecycle
+
+        private void FixedUpdate()
+        {
+            if (_target.Value != null && _transform.Value != null)
+            {
+                Quaternion rotation = _readLocalRotation ? _target.Value.localRotation : _target.Value.rotation;
+                if (_setLocalRotation)
+                {
+                    _transform.Value.localRotation = rotation;
+                }
+                else
+                {
+                    _transform.Value.rotation = rotation;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
