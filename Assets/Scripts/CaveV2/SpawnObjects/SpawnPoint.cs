f87b7d9789de46e4b84bcaa71338feef1989881f
Max Lepkowski <mlepkows@stevens.edu>
2023-08-30 18:42:48 -0400

    Organize spawn point inspector, add tooltips.

--- Assets/Scripts/CaveV2/SpawnObjects/SpawnPoint.cs
+++ Assets/Scripts/CaveV2/SpawnObjects/SpawnPoint.cs
@@ -23,8 +23,47 @@ namespace BML.Scripts.CaveV2.SpawnObjects
         }
         
         #region Inspector
+        
+        [ShowInInspector, ReadOnly, InfoBox("This spawn point will not be found if it's untagged!", InfoMessageType.Error, "@_isUntagged"), PropertyOrder(-1)] 
+        private string _tag => this.tag;
+        private bool _isUntagged => _tag == null || _tag == "Untagged";
 
-        [ShowInInspector, ReadOnly]
+        [TitleGroup("Projection behavior")]
+        [SerializeField, HideLabel] private SpawnPointProjectionBehavior _projectionBehavior;
+        [SerializeField] private LayerMask _projectionLayerMask;
+        [SerializeField, ShowIf("@this._projectionBehavior == SpawnPointProjectionBehavior.Vector || this._projectionBehavior == SpawnPointProjectionBehavior.Randomize")] 
+        private Vector3 _projectionVector = Vector3.down;
+        [SerializeField, ShowIf("@this._projectionBehavior == SpawnPointProjectionBehavior.Randomize")] 
+        private Vector2 _projectionDirectionRandomnessRangeDegrees = new Vector2(30, 15);
+        [SerializeField] private float _projectionDistance = 45f;
+
+        [SerializeField] private bool _doInheritParentRotation = true;
+        [SerializeField] private bool _rotateTowardsSurfaceNormal = true;
+        [SerializeField] private Vector3 _rotationEulerOffset = Vector3.zero;
+        
+        [SerializeField, ReadOnly] private Vector3? _projectedPosition = null; 
+        [SerializeField, ReadOnly] private Quaternion? _projectedRotation = null; 
+        
+        [TitleGroup("Spawn behavior")]
+        [SerializeField] [Range(0f, 1f)] private float _startingSpawnChance = 1f;
+        [SerializeField] private bool _disableIfPlayerVisited = false;
+        [SerializeField, Tooltip("If the last enemy to occupy this spawn point was despawned, this will ensure they get respawned immediately when the spawn point is active again.")] 
+        private bool _persistSpawns = false;
+        [ShowInInspector, ReadOnly] private bool _previousSpawnWasDespawned = false;
+        [SerializeField, Tooltip("Guarantees this spawn point will be populated as soon as it becomes active.")] 
+        private bool _guaranteeSpawnIfInRange = false;
+        [SerializeField, Tooltip("Prevents more than one enemy from spawning here at a time. No other enemy can spawn until the occupier either dies or despawns.")] 
+        private bool _limitToOneSpawnAtATime = false;
+        [SerializeField, Tooltip("Limit the number of enemies this spawner can produce for it's lifetime.")] 
+        private int _spawnLimit = -1;
+        [ShowInInspector, ReadOnly] private int _spawnCount = 0;
+        [SerializeField, Tooltip("If enabled, the above 'spawn limit' will only be applied to spawns from 'guarantee spawn if in range'; after the limit is reached guaranteed spawns will cease, but the spawn point will still be available for the EnemySpawnManager to repopulate through it's normal spawning routine.")] 
+        private bool _applySpawnLimitOnlyToGuaranteedSpawns = false;
+        [SerializeField, Tooltip("Ignore the EnemySpawnManager's global concurrent spawn limit.")] public bool IgnoreGlobalSpawnCap = false;
+
+        [FoldoutGroup("Debug"), SerializeField] private bool _showDebugPrefab;
+        [FoldoutGroup("Debug"), SerializeField] private GameObject _debugPrefab;
+        [FoldoutGroup("Debug"), ShowInInspector, ReadOnly] private ICaveNodeData parentNode;
         public ICaveNodeData ParentNode
         {
             get => parentNode;
@@ -36,56 +75,20 @@ namespace BML.Scripts.CaveV2.SpawnObjects
                 _projectedRotation = null;
             }
         }
-
-        [SerializeField] private SpawnPointProjectionBehavior _projectionBehavior;
-        [SerializeField] private LayerMask _projectionLayerMask;
-        [SerializeField, ShowIf("@this._projectionBehavior == SpawnPointProjectionBehavior.Vector || this._projectionBehavior == SpawnPointProjectionBehavior.Randomize")] 
-        private Vector3 _projectionVector = Vector3.down;
-        [SerializeField, ShowIf("@this._projectionBehavior == SpawnPointProjectionBehavior.Randomize")] 
-        private Vector2 _projectionDirectionRandomnessRangeDegrees = new Vector2(30, 15);
-        [SerializeField] private float _projectionDistance = 45f;
-
-        [SerializeField] private bool _doInheritParentRotation = true;
-        [SerializeField] private bool _rotateTowardsSurfaceNormal = true;
-        [SerializeField] private Vector3 _rotationEulerOffset = Vector3.zero;
-        [SerializeField, ReadOnly] private Vector3? _projectedPosition = null; 
-        [SerializeField, ReadOnly] private Quaternion? _projectedRotation = null; 
         
-        [SerializeField] [Range(0f, 1f)] private float _startingSpawnChance = 1f;
-        [SerializeField] private bool _disableIfPlayerVisited;
-
-        [SerializeField] private bool _persistSpawns = false;
-        [ShowInInspector, ReadOnly] private bool _previousSpawnWasDespawned = false;
-        [SerializeField] private bool _guaranteeSpawnIfInRange = false;
-        [SerializeField] private bool _limitToOneSpawnAtATime = false;
-        [SerializeField] private int _spawnLimit = -1;
-        [SerializeField] private bool _applySpawnLimitOnlyToGuaranteedSpawns = false;
-        [ShowInInspector, ReadOnly] private int _spawnCount = 0;
-        [SerializeField] public bool IgnoreGlobalSpawnCap = false;
-        
-        [ShowInInspector, ReadOnly] public bool Occupied;
-
-        [ShowInInspector, ReadOnly]
+        [FoldoutGroup("Current state"), ShowInInspector, ReadOnly] public bool Occupied;
+        [FoldoutGroup("Current state"), ShowInInspector, ReadOnly]
         public float SpawnChance
         {
             get => _spawnChance * (!_applySpawnLimitOnlyToGuaranteedSpawns && _spawnLimit > -1 && _spawnCount >= _spawnLimit ? 0 : 1);
             set => _spawnChance = value;
         }
-
         private float _spawnChance = 1f;
-        [ShowInInspector, ReadOnly] public bool SpawnImmediate => ((_guaranteeSpawnIfInRange && (_spawnLimit <= -1 || _spawnLimit > _spawnCount)) 
+        
+        [FoldoutGroup("Current state"), ShowInInspector, ReadOnly] public bool SpawnImmediate => ((_guaranteeSpawnIfInRange && (_spawnLimit <= -1 || _spawnLimit > _spawnCount)) 
                                                                    || (_persistSpawns && _previousSpawnWasDespawned));
-        [ShowInInspector, ReadOnly] public float EnemySpawnWeight { get; set; } = 1f;
-        
-        [SerializeField] private bool _showDebugPrefab;
-        [SerializeField] private GameObject _debugPrefab;
+        [FoldoutGroup("Current state"), ShowInInspector, ReadOnly] public float EnemySpawnWeight { get; set; } = 1f;
 
-        private ICaveNodeData parentNode;
-
-        // TODO fetch current object tag and display it. Show a warning/error if object is NOT tagged.
-        [ShowInInspector, ReadOnly, InfoBox("", InfoMessageType.Error, "@this.tag")] 
-        private string _tag => this.tag;
-        
         #endregion
 
         #region Unity lifecycle
@@ -165,8 +168,8 @@ namespace BML.Scripts.CaveV2.SpawnObjects
             // TODO calculate base on object parameters
         }
 
-        [Button]
-        private void Test()
+        [TitleGroup("Projection behavior"), Button]
+        private void TestProjection()
         {
             Project();
         }
