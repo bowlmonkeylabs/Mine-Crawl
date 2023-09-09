e9f981feff7a027aca24f923fea761e2b095d208
Dan C <djc5627@gmail.com>
2023-09-09 14:35:27 -0400

    Fix errors after moving graph scripts to new folder.

--- Assets/Scripts/ItemTreeGraph/ItemTreeGraph.cs
+++ Assets/Scripts/ItemTreeGraph/ItemTreeGraph.cs
@@ -16,7 +16,7 @@ namespace BML.Scripts.Player.Items
 
     }
 
-    [CreateAssetMenu(fileName = "ItemTreeGraph", menuName = "BML/Player/ItemTreeGraph", order = 0)]
+    [CreateAssetMenu(fileName = "ItemTreeGraph", menuName = "BML/Graphs/ItemTreeGraph", order = 0)]
     public class ItemTreeGraph : NodeGraph, IResettableScriptableObject {
         public List<PlayerItem> GetUnobtainedItemPool() {
             var unobtainedItemPool = new List<PlayerItem>();
