4df51e68abb1acfdbc9147d3c67d118468e074d5
Max Lepkowski <mlepkows@stevens.edu>
2023-09-11 17:31:00 -0400

    Update player inventory to check item type.

--- Assets/Scripts/Player/Items/PlayerInventory.cs
+++ Assets/Scripts/Player/Items/PlayerInventory.cs
@@ -12,32 +12,85 @@ namespace BML.Scripts.Player.Items
     [CreateAssetMenu(fileName = "PlayerInventory", menuName = "BML/Player/PlayerInventory", order = 0)]
     public class PlayerInventory : ScriptableObject, IResettableScriptableObject
     {
-        [SerializeField] private PlayerItem _activeItem;
-        [SerializeField] private PlayerItem _passiveItem;
+        #region Inspector
+
+        [SerializeField, OnValueChanged("OnActiveItemChanged"), InfoBox("Item is not of type 'Active'", InfoMessageType.Error, "@_activeItem != null && _activeItem.Type != ItemType.Active")] private PlayerItem _activeItem;
+        private void OnActiveItemChanged()
+        {
+            OnActiveItemAdded?.Invoke(_activeItem);
+        }
+        
+        [SerializeField, InfoBox("Item is not of type 'Passive'", InfoMessageType.Error, "@_passiveItem != null && _passiveItem.Type != ItemType.Passive")] private PlayerItem _passiveItem;
+        
         [SerializeField] private List<PlayerItem> _passiveStackableItems;
 
-        public PlayerItem ActiveItem {
+        #endregion
+
+        #region Public interface
+
+        public PlayerItem ActiveItem 
+        {
             get => _activeItem;
             set {
-                if(value != null) {
-                    OnActiveItemRemoved.Invoke(_passiveItem);
+                if (value.Type == ItemType.Active)
+                {
+                    if (_activeItem != null) OnActiveItemRemoved?.Invoke(_activeItem);
+                    _activeItem = value;
+                    if (value != null) OnActiveItemAdded?.Invoke(value);
                 }
-                _activeItem = value;
-                OnActiveItemAdded.Invoke(value);
             }
         }
-        public PlayerItem PassiveItem {
+        
+        public PlayerItem PassiveItem 
+        {
             get => _passiveItem;
             set {
-                if(value != null) {
-                    OnPassiveItemRemoved.Invoke(_passiveItem);
+                if (value.Type == ItemType.Passive)
+                {
+                    if (_passiveItem != null) OnPassiveItemRemoved?.Invoke(_passiveItem);
+                    _passiveItem = value;
+                    if (value != null) OnPassiveItemAdded?.Invoke(value);
                 }
-                _passiveItem = value;
-                OnPassiveItemAdded.Invoke(value);
             }
         }
-        public List<PlayerItem> PassiveStackableItems { get => _passiveStackableItems; }
         
+        public List<PlayerItem> PassiveStackableItems => _passiveStackableItems;
+
+        public void AddPassiveStackableItem(PlayerItem playerItem) 
+        {
+            if (playerItem.Type == ItemType.PassiveStackable)
+            {
+                _passiveStackableItems.Add(playerItem);
+                OnPassiveStackableItemAdded?.Invoke(playerItem);
+            }
+        }
+
+        public void RemovePassiveStackableItem(PlayerItem playerItem) 
+        {
+            if (playerItem.Type == ItemType.PassiveStackable)
+            {
+                bool didRemove = _passiveStackableItems.Remove(playerItem);
+                if (didRemove)
+                {
+                    OnPassiveStackableItemAdded?.Invoke(playerItem);
+                }
+            }
+        }
+
+        public void ResetScriptableObject()
+        {
+            this._activeItem = null;
+            this._passiveItem = null;
+            this._passiveStackableItems.Clear();
+
+            OnActiveItemAdded?.Invoke(null);
+            OnPassiveItemAdded?.Invoke(null);
+            OnPassiveStackableItemAdded?.Invoke(null);
+        }
+
+        #endregion
+
+        #region Events
 
         public event OnPlayerItemChanged<PlayerItem> OnPassiveStackableItemAdded;
         public event OnPlayerItemChanged<PlayerItem> OnPassiveStackableItemRemoved;
@@ -46,21 +99,7 @@ namespace BML.Scripts.Player.Items
         public event OnPlayerItemChanged<PlayerItem> OnActiveItemAdded;
         public event OnPlayerItemChanged<PlayerItem> OnActiveItemRemoved;
 
-        public void AddPassiveStackableItem(PlayerItem playerItem) {
-            _passiveStackableItems.Add(playerItem);
-            OnPassiveStackableItemAdded.Invoke(playerItem);
-        }
+        #endregion
 
-        public void RemovePassiveStackableItem(PlayerItem playerItem) {
-            _passiveStackableItems.Remove(playerItem);
-            OnPassiveStackableItemRemoved.Invoke(playerItem);
-        }
-
-        public void ResetScriptableObject()
-        {
-            this._activeItem = null;
-            this._passiveItem = null;
-            this._passiveStackableItems.Clear();
-        }
     }
 }
