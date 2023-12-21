454a8fb91779ccf89e91434ce477e8262aa1e0b7
Max Lepkowski <mlepkows@stevens.edu>
2023-12-21 18:46:58 -0500

    Add logs to GameEvent.

--- Assets/BML/ScriptableObjectCore/Scripts/ScriptableVariableBase.cs
+++ Assets/BML/ScriptableObjectCore/Scripts/ScriptableVariableBase.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
 
 namespace BML.ScriptableObjectCore.Scripts
 {
     public class ScriptableVariableBase : ScriptableObject
     {
-        
+        private Color _colorEnableLogs => enableLogs ? Color.yellow : Color.gray;
+        [InfoBox("Logging is enabled for this scriptable object", InfoMessageType.Warning, visibleIfMemberName:"enableLogs")]
+        [SerializeField, HideInInlineEditors, GUIColor("_colorEnableLogs")] protected bool enableLogs;
     }
 }
