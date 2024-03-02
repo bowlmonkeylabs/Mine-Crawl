8b5fb7a34905da1edd755d664c5aa95512b6ed26
Max Lepkowski <mlepkows@stevens.edu>
2024-03-02 13:49:00 -0500

    Add Player_FireBomb damage type; weak against ore/rocks. Implement 'firecracker': fire bomb damage, small radius, explode on contact.

--- Assets/Scripts/Damageable/DamageType.cs
+++ Assets/Scripts/Damageable/DamageType.cs
@@ -1,21 +1,22 @@
 namespace BML.Scripts {
     public enum DamageType
     {
-        Player_Pickaxe = 1,
-        Player_Bomb = 2,
-        Enemy_Slime_Contact = 4,
-        Enemy_Bat_Contact = 8,
-        Enemy_Sentinel_Projectile = 16,
-        Enemy_Zombie_Contact = 32,
-        Enemy_Mimic_Projectile = 64,
-        Ore_Explosive = 128,
-        Enemy_Snake_Projectile = 256,
-        Fall_Damage = 512,
-        Player_Pickaxe_Secondary = 1024,
-        Enemy_Bomb_Explosion = 2048,
-        Trap_Spike = 4096,
-        Trap_Stalactite = 8192,
-        Enemy_Worm = 16384,
-        Player_Projectile = 32768
+        Player_Pickaxe = 1 << 0,
+        Player_Bomb = 1 << 1,
+        Enemy_Slime_Contact = 1 << 2,
+        Enemy_Bat_Contact = 1 << 3,
+        Enemy_Sentinel_Projectile = 1 << 4,
+        Enemy_Zombie_Contact = 1 << 5,
+        Enemy_Mimic_Projectile = 1 << 6,
+        Ore_Explosive = 1 << 7,
+        Enemy_Snake_Projectile = 1 << 8,
+        Fall_Damage = 1 << 9,
+        Player_Pickaxe_Secondary = 1 << 10,
+        Enemy_Bomb_Explosion = 1 << 11,
+        Trap_Spike = 1 << 12,
+        Trap_Stalactite = 1 << 13,
+        Enemy_Worm = 1 << 14,
+        Player_Projectile = 1 << 15,
+        Player_FireBomb = 1 << 16,
     }
 }
