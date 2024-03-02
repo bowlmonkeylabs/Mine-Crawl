eb843fed33469a27f36416ad48841f8bc86995b1
Max Lepkowski <mlepkows@stevens.edu>
2024-03-02 13:44:19 -0500

    Add option to limit ThrowItemEffect to one at a time (can't throw until last is destroyed).

--- Assets/Scripts/Player/Items/ItemEffects/ThrowItemEffect.cs
+++ Assets/Scripts/Player/Items/ItemEffects/ThrowItemEffect.cs
@@ -2,6 +2,7 @@ using BML.ScriptableObjectCore.Scripts.SceneReferences;
 using BML.ScriptableObjectCore.Scripts.Variables.SafeValueReferences;
 using BML.Scripts.Utils;
 using Sirenix.OdinInspector;
+using Sirenix.Utilities;
 using UnityEngine;
 
 namespace BML.Scripts.Player.Items.ItemEffects
@@ -14,20 +15,30 @@ namespace BML.Scripts.Player.Items.ItemEffects
         public GameObject Prefab;
         public SafeFloatValueReference ThrowForce;
         public TransformSceneReference Container;
+        
+        [SerializeField, Tooltip("Prevents throwing another item while the last thrown still exists.")] 
+        private bool _onlyOneAtATime = false;
 
         #endregion
+
+        private Throwable _lastThrown;
 
         private Transform _firePoint;
         public void PrimeEffect(Transform firePoint)
         {
             _firePoint = firePoint;
         }
-        
+
         protected override bool ApplyEffectInternal()
         {
+            if (_onlyOneAtATime && !_lastThrown.SafeIsUnityNull())
+            {
+                return false;
+            }
+            
             // Calculate throw
             var throwDir = _firePoint.forward;
-            // TODO make throw juicier. have throw angle affect the resulting object rotation/spin?
+            // TODO make throw juicier. have throw angle affect the resulting object rotation/spin? potentially you place when the ground is in range, but throw when it's further away?
             var throwForce = throwDir * ThrowForce.Value;
             
             // Instantiate throwable
@@ -35,6 +46,7 @@ namespace BML.Scripts.Player.Items.ItemEffects
             newGameObject.transform.SetPositionAndRotation(_firePoint.position, _firePoint.rotation);
             var throwable = newGameObject.GetComponentInChildren<Throwable>();
             throwable.DoThrow(throwForce);
+            _lastThrown = throwable;
             
             return true;
         }
