8a73423f0696a4dd3219f1ba63a898a4f07e654b
Max Lepkowski <mlepkows@stevens.edu>
2024-03-02 13:50:00 -0500

    Remove redundant 'map range' util function.

--- Assets/Scripts/Utils/FloatUtils.cs
+++ Assets/Scripts/Utils/FloatUtils.cs
@@ -9,12 +9,13 @@ namespace BML.Scripts.Utils
             return Mathf.Abs(val) < 0.001f ? 0 : val;
         }
 
-        public static float RemapRange(float value, float oldMin, float oldMax, float newMin, float newMax)
+        public static float RemapRange(float value, float oldMin, float oldMax, float newMin, float newMax, bool clamp = false)
         {
             float oldRange = (oldMax - oldMin);
             float pct = Mathf.Approximately(oldRange, 0) 
                 ? 0 : (value - oldMin) / oldRange;
             float newValue = pct * (newMax - newMin) + newMin;
+            newValue = (clamp ? Mathf.Clamp(newValue, newMin, newMax) : newValue);
             return newValue;
         }
     }
