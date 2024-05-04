ba76ac47485f422af5756f742506a45cf34c2d8f
Max Lepkowski <mlepkows@stevens.edu>
2024-05-04 11:31:33 -0400

    Warnings cleanup.

--- Assets/Scripts/Utils/MeshUtils.cs
+++ Assets/Scripts/Utils/MeshUtils.cs
@@ -17,18 +17,18 @@ namespace BML.Scripts.Utils
             mesh.normals = mesh.normals.Select(n => -n).ToArray();
             return;
             
-            // TODO consider doing this in place on the mesh data, with no memory allocations for the temp arrays used below
+            #warning consider doing this in place on the mesh data, with no memory allocations for the temp arrays used below
             // Invert normals
-            Vector3[] normals = mesh.normals;
-            for (int i = 0; i < normals.Length; i++)
-            {
-                normals[i] = -normals[i];
-            }
-            // Assign inverted normals back to input mesh
-            // mesh.normals = normals;
-            Debug.Log($"{mesh.normals[0]} | {normals[0]}");
-            mesh.SetNormals(normals);
-            Debug.Log($"{mesh.normals[0]} | {normals[0]}");
+            // Vector3[] normals = mesh.normals;
+            // for (int i = 0; i < normals.Length; i++)
+            // {
+            //     normals[i] = -normals[i];
+            // }
+            // // Assign inverted normals back to input mesh
+            // // mesh.normals = normals;
+            // Debug.Log($"{mesh.normals[0]} | {normals[0]}");
+            // mesh.SetNormals(normals);
+            // Debug.Log($"{mesh.normals[0]} | {normals[0]}");
     
             // int[] triangles = mesh.triangles;
             // for (int i = 0; i < triangles.Length; i++)
